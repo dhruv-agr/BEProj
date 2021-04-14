3bdbedaf3f537874542bf83f09edd759700bc673
Dhruv <dhruvagr15@gmail.com>
2021-04-14 22:30:23 +0530

    Removed http prefix from ALLOWED_HOSTS

--- bugTrackingSystem/settings.py
+++ bugTrackingSystem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','=rhy9&$t7@2x8yi*n)t=@9xg&r&87@t
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://bugtrackingsystembe.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['bugtrackingsystembe.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
